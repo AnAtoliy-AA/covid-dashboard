74d320ec1c86f2a4c6128e27fdceddf205192fc9
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-12-09 22:36:16 +0300

    feat: create main components

---
+++ covid-dashboard/src/components/CountryLevel/CountryLevel.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import styles from './CountryLevel.module.css';
+
+export default class CountryLevel extends Component {
+    render() {
+        return(
+         <div className={styles.countryLevel}>CountryLevel</div>
+        )
+      }
+}
---
+++ covid-dashboard/src/components/GlobalDeaths/GlobalDeaths.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import styles from './GlobalDeaths.module.css';
+
+export default class GlobalDeaths extends Component {
+    render() {
+        return(
+         <div className={styles.globalDeaths}>GlobalDeaths</div>
+        )
+      }
+}
