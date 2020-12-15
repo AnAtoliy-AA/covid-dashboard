b11e33bb191bcd3f1c3c732933673af9bcffe163
khovricheva <kseniyap1102@gmail.com>
2020-12-15 17:30:27 +0200

    feat: add list of countries

--- covid-dashboard/src/components/CountryList/CountrySearch/CountrySearch.jsx
+++ covid-dashboard/src/components/CountryList/CountrySearch/CountrySearch.jsx
@@ -3,6 +3,29 @@ import styles from './CountrySearch.module.scss';
 
 export default class CountrySearch extends Component {
   render() {
-    return <div className={styles.countrySearch}></div>;
+    return (
+      <div className={styles.countrySearch}>
+        <input
+          type='text'
+          className={styles.search}
+          placeholder='Search for a Country'
+        />
+        <div>
+          <ul className={styles.countries}>
+            {this.props.countryList.map((country) => {
+              return (
+                <li key={country.CountryCode} className={styles.countryItem}>
+                  <img
+                    src={`https://www.countryflags.io/${country.CountryCode}/shiny/64.png`}
+                    className={styles.countryItem_flag}
+                  />
+                  <h3 className={styles.countryItem_name}>{country.Country}</h3>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
