74b49eabd005a83884ae739534bdebb917168d52
khovricheva <kseniyap1102@gmail.com>
2020-12-15 17:36:02 +0200

    feat: add ability to select country on input

--- covid-dashboard/src/components/CountryList/CountrySearch/CountrySearch.jsx
+++ covid-dashboard/src/components/CountryList/CountrySearch/CountrySearch.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import styles from './CountrySearch.module.scss';
 
 export default class CountrySearch extends Component {
+  state = {
+    searchTerm: '',
+  };
+
+  selectCountry(country) {
+    this.setState({ searchTerm: country });
+  }
+
   render() {
     return (
       <div className={styles.countrySearch}>
@@ -9,19 +17,31 @@ export default class CountrySearch extends Component {
           type='text'
           className={styles.search}
           placeholder='Search for a Country'
+          value={this.state.searchTerm}
+          onInput={(event) => this.selectCountry(event.target.value)}
         />
-        <div>
+        <div
+          className={
+            this.state.searchTerm.length !== 0
+              ? styles.results
+              : styles.results__hide
+          }
+        >
           <ul className={styles.countries}>
             {this.props.countryList.map((country) => {
-              return (
-                <li key={country.CountryCode} className={styles.countryItem}>
-                  <img
-                    src={`https://www.countryflags.io/${country.CountryCode}/shiny/64.png`}
-                    className={styles.countryItem_flag}
-                  />
-                  <h3 className={styles.countryItem_name}>{country.Country}</h3>
-                </li>
-              );
+              if (country.Slug.includes(this.state.searchTerm.toLowerCase())) {
+                return (
+                  <li key={country.CountryCode} className={styles.countryItem}>
+                    <img
+                      src={`https://www.countryflags.io/${country.CountryCode}/shiny/64.png`}
+                      className={styles.countryItem_flag}
+                    />
+                    <h3 className={styles.countryItem_name}>
+                      {country.Country}
+                    </h3>
+                  </li>
+                );
+              }
             })}
           </ul>
         </div>
