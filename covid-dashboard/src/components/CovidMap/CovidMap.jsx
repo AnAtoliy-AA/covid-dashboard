5d2a632e726de62bd587058ac645c854ccf80713
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-12-20 09:47:48 +0300

    feat: add styles to covid map

--- covid-dashboard/src/components/CovidMap/CovidMap.jsx
+++ covid-dashboard/src/components/CovidMap/CovidMap.jsx
@@ -1,11 +1,24 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import React, { Component } from 'react';
 
 import styles from './CovidMap.module.css';
 
 export default class CovidMap extends Component {
-    render() {
-        return(
-         <div className={styles.covidMap}>CovidMap</div>
-        )
-      }
+  render() {
+    return (
+      <div className={styles.covidMap}>
+        <MapContainer  className={styles.leafletContainer} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    )
+  }
 }
