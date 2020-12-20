3c9af2ac5ecf1815d51dd6187c511bea7da6f4da
khovricheva <kseniyap1102@gmail.com>
2020-12-20 21:10:15 +0200

    feat: add global cases graph

--- covid-dashboard/src/components/CovidGraph/CovidGraph.jsx
+++ covid-dashboard/src/components/CovidGraph/CovidGraph.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import { Bar, Line } from 'react-chartjs-2';
 
 import styles from './CovidGraph.module.scss';
 
 export default class CovidGraph extends Component {
-    render() {
-        return(
-         <div className={styles.covidGraph}>CovidGraph</div>
-        )
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      globalCases: {
+        chartData: {},
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.getGlobalCasesData();
+  }
+
+  getGlobalCasesData(indicator = 'cases') {
+    axios
+      .get('https://disease.sh/v3/covid-19/historical/all?lastdays=366')
+      .then((response) => {
+        this.setState({
+          globalCases: {
+            chartData: {
+              labels: Object.keys(response.data[indicator]),
+              datasets: [
+                {
+                  label: 'Cases',
+                  data: Object.values(response.data[indicator]).map(
+                    (item) => item / 1000000
+                  ),
+                  backgroundColor: ['rgba(255, 255, 255, 0.2)'],
+                  borderColor: ['rgba(255, 99, 132, 1)'],
+                  borderWidth: 1,
+                },
+              ],
+            },
+          },
+        });
+      });
+  }
+
+  render() {
+    console.log(this.props.activeCountry);
+
+    return (
+      <div className={styles.covidGraph}>
+        <Line data={this.state.globalCases.chartData} />
+      </div>
+    );
+  }
 }
---
+++ covid-dashboard/src/components/CovidGraph/CovidGraphContainer.jsx
@@ -0,0 +1,15 @@
+import CovidGraph from './CovidGraph';
+import { connect } from 'react-redux';
+// import { setActiveCountryActionCreator } from '../../../redux/countryList-reducer';
+
+let mapStateToProps = (state) => {
+  return {
+    worldWideData: state.countryList.worldWideData,
+    activeCountry: state.countryList.activeCountry,
+  };
+};
+let mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CovidGraph);
