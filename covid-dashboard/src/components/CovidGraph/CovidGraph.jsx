43f7f3131a6484d8af832b149b6af27ed0b0a4ac
khovricheva <kseniyap1102@gmail.com>
2020-12-20 21:47:54 +0200

    feat: add country cases graph

--- covid-dashboard/src/components/CovidGraph/CovidGraph.jsx
+++ covid-dashboard/src/components/CovidGraph/CovidGraph.jsx
@@ -16,10 +16,20 @@ export default class CovidGraph extends Component {
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
+    console.log(this.props.activeCountry);
     this.getGlobalCasesData();
   }
 
+  componentDidUpdate(prevProps) {
+    console.log('componentDidUpdate');
+    if (this.props.activeCountry.Slug !== prevProps.activeCountry.Slug)
+      this.getCountryCasesData();
+  }
+
   getGlobalCasesData(indicator = 'cases') {
+    console.log('global');
+
     axios
       .get('https://disease.sh/v3/covid-19/historical/all?lastdays=366')
       .then((response) => {
@@ -29,7 +39,7 @@ export default class CovidGraph extends Component {
               labels: Object.keys(response.data[indicator]),
               datasets: [
                 {
-                  label: 'Cases',
+                  label: 'Global cases',
                   data: Object.values(response.data[indicator]).map(
                     (item) => item / 1000000
                   ),
@@ -44,12 +54,74 @@ export default class CovidGraph extends Component {
       });
   }
 
-  render() {
-    console.log(this.props.activeCountry);
+  getCountryCasesData(indicator = 'confirmed') {
+    console.log('country');
 
+    console.log(this.props.activeCountry.Slug);
+    console.log(indicator);
+
+    axios
+      .get(
+        `https://api.covid19api.com/dayone/country/${this.props.activeCountry.Slug}/status/${indicator}`
+      )
+      .then((response) => {
+        this.setState({
+          globalCases: {
+            chartData: {
+              labels: response.data.map((item) =>
+                new Date(item.Date).toString().substring(4, 15)
+              ),
+              datasets: [
+                {
+                  label: ' Country cases',
+                  data: response.data.map((item) => item.Cases),
+                  backgroundColor: ['rgba(255, 255, 255, 0.2)'],
+                  borderColor: ['rgba(255, 99, 132, 1)'],
+                  borderWidth: 1,
+                },
+              ],
+            },
+          },
+        });
+      });
+  }
+
+  render() {
     return (
       <div className={styles.covidGraph}>
         <Line data={this.state.globalCases.chartData} />
+        <div>
+          <button
+            id='cases'
+            onClick={(event) =>
+              this.props.activeCountry
+                ? this.getCountryCasesData(event.target.innerText)
+                : this.getGlobalCasesData(event.target.id)
+            }
+          >
+            confirmed
+          </button>
+          <button
+            id='deaths'
+            onClick={(event) =>
+              this.props.activeCountry
+                ? this.getCountryCasesData(event.target.id)
+                : this.getGlobalCasesData(event.target.id)
+            }
+          >
+            deaths
+          </button>
+          <button
+            id='recovered'
+            onClick={(event) =>
+              this.props.activeCountry
+                ? this.getCountryCasesData(event.target.id)
+                : this.getGlobalCasesData(event.target.id)
+            }
+          >
+            recovered
+          </button>
+        </div>
       </div>
     );
   }
--- covid-dashboard/src/components/CovidGraph/CovidGraphContainer.jsx
+++ covid-dashboard/src/components/CovidGraph/CovidGraphContainer.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 let mapStateToProps = (state) => {
   return {
-    worldWideData: state.countryList.worldWideData,
+    // worldWideData: state.countryList.worldWideData,
     activeCountry: state.countryList.activeCountry,
   };
 };
