de4c4990c4104337fad54567571ded247a6c926f
AnAtoliyAK <aliaksandrau89@gmail.com>
2020-12-20 16:12:09 +0300

    feat: add flag to popup

--- covid-dashboard/src/components/CovidMap/CovidMap.jsx
+++ covid-dashboard/src/components/CovidMap/CovidMap.jsx
@@ -8,7 +8,7 @@ export default class CovidMap extends Component {
   render() {
     return (
       <div className={styles.covidMap}>
-        <MapContainer className={styles.leafletContainer} center={[48.38, 31.17]} zoom={3} scrollWheelZoom={false}>
+        <MapContainer className={styles.leafletContainer} center={[48.38, 31.17]} zoom={4} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -17,7 +17,8 @@ export default class CovidMap extends Component {
             return (
               <Marker position={[c.countryInfo.lat, c.countryInfo.long]}>
                 <Popup>
-                  {c.country} <br /> {c.cases}
+                <img className={styles.countryFlag} src={c.countryInfo.flag} alt=""/>
+                  {c.country} <br /> {c.cases}     
     </Popup>
               </Marker>
             )
