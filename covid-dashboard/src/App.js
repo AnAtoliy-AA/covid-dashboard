58ce05ae3e486118a4c6f368754ea0266f4514f1
khovricheva <kseniyap1102@gmail.com>
2020-12-22 18:31:13 +0200

    fix: resolve conflicts

--- covid-dashboard/src/App.js
+++ covid-dashboard/src/App.js
@@ -1,21 +1,12 @@
 import './App.scss';
 
 import CountryListContainer from './components/CountryList/CountryListContainer';
-<<<<<<< HEAD
-import CovidGraph from './components/CovidGraph/CovidGraph';
 import CovidMapContainer from './components/CovidMap/CovidMapContainer';
-=======
 import CovidGraphContainer from './components/CovidGraph/CovidGraphContainer';
-import CovidMap from './components/CovidMap/CovidMap';
->>>>>>> 44642e172c2f104086fe6aea31035102a40fdcd5
 import CovidTableContainer from './components/CovidTable/CovidTableContainer';
-import FooterContainer from './components/Footer/FooterContainer'
+import FooterContainer from './components/Footer/FooterContainer';
 import GlobalCasesContainer from './components/GlobalCases/GlobalCasesContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
-<<<<<<< HEAD
-=======
-import FooterContainer from './components/Footer/FooterContainer';
->>>>>>> 44642e172c2f104086fe6aea31035102a40fdcd5
 import React from 'react';
 
 function App() {
@@ -26,13 +17,8 @@ function App() {
         <GlobalCasesContainer />
         <CountryListContainer />
         <CovidTableContainer />
-<<<<<<< HEAD
         <CovidMapContainer />
-        <CovidGraph />
-=======
-        <CovidMap />
         <CovidGraphContainer />
->>>>>>> 44642e172c2f104086fe6aea31035102a40fdcd5
       </div>
       <FooterContainer />
     </div>
