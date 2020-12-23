9820619e621082e02d85bda7772f4215e5725feb
Mariia Derkach <mary.moss333@gmail.com>
2020-12-23 17:23:43 +0200

    feat:add switcher

--- covid-dashboard/src/components/CovidTable/CountryLevel/CountryLevel.jsx
+++ covid-dashboard/src/components/CovidTable/CountryLevel/CountryLevel.jsx
@@ -5,31 +5,39 @@ import styles from './CountryLevel.module.scss';
 export default class CountryLevel extends Component {
 
   render() {
-    return (
-      <div className={styles.countryLevel}>
-        <div>
-          <div>
-            {(this.props.isCountrySelected)
-              ? <div className={styles.styleConfirmed}>{this.props.activeCountry.Country} confirmed :
-                <div className={styles.confirmed}>{this.props.activeCountry.TotalConfirmed}</div>
+
+    if(this.props.checked===false){
+      return(     
+      <div className={styles.countryDeaths}>
+          <div className={styles.styleConfirmed}>New {this.props.activeCountry.Country} confirmed :
+             {(this.props.isCountrySelected)
+             ? <div className={styles.confirmed}>
+                 {this.props.activeCountry.NewConfirmed}
               </div>
-              : <div className={styles.styleConfirmed}>World confirmed :
-                <div className={styles.confirmed}>{this.props.covidTableWorldWideData.TotalConfirmed}</div>
-              </div>
-            }
-          </div>
-          <div className={styles.styleConfirmed}>New confirmed :
-            {(this.props.isCountrySelected)
-              ? <div className={styles.confirmed}>
-                {this.props.activeCountry.NewConfirmed}
-              </div>
-              : <div className={styles.confirmed}>
+             : <div className={styles.confirmed}>
                 {this.props.covidTableWorldWideData.NewConfirmed}
-              </div>
-            }
-          </div>
+             </div>
+             }
+           </div>
+      </div>)
+    } else {
+      return (
+        <div className={styles.countryLevel}>
+            <div>
+              {(this.props.isCountrySelected)
+                ? <div className={styles.styleConfirmed}>{this.props.activeCountry.Country} Confirmed :
+                  <div className={styles.confirmed}>{this.props.activeCountry.TotalConfirmed}</div>
+                </div>
+                : <div className={styles.styleConfirmed}>World confirmed :
+                  <div className={styles.confirmed}>{this.props.covidTableWorldWideData.TotalConfirmed}</div>
+                </div>
+              }
+            </div>
         </div>
-      </div>
-    )
+      )
+    }
+
+
+    
   }
 }
